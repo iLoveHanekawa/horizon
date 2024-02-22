dcf1ab490c99a640e2602fd35d4b4a3004e65091
iLoveHanekawa <arjunthakur900@gmail.com>
2024-02-22 20:17:14 +0530

    fix[auth]: added required fields to the registration form

--- src/app/admin/(authentication)/register/page.tsx
+++ src/app/admin/(authentication)/register/page.tsx
@@ -20,27 +20,27 @@ export default function Page() {
             <form action={dispatch} className="mt-5 flex flex-col gap-4">
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                     <Label htmlFor="firstname">First name</Label>
-                    <Input data-testid="firstname" id="firstname" type={'text'} name="firstname" placeholder="John" />
+                    <Input required={true} data-testid="firstname" id="firstname" type={'text'} name="firstname" placeholder="John" />
                     <Errors errors={state.errors.fieldErrors.firstname} />
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                     <Label htmlFor="lastname">Last name</Label>
-                    <Input data-testid="lastname" id="lastname" type={'text'} name="lastname" placeholder="Doe" />
+                    <Input required={true} data-testid="lastname" id="lastname" type={'text'} name="lastname" placeholder="Doe" />
                     <Errors errors={state.errors.fieldErrors.lastname} />
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                     <Label htmlFor="email">Email</Label>
-                    <Input data-testid="email" id="email" type={'email'} name="email" placeholder="john.doe@example.com" />
+                    <Input required={true} data-testid="email" id="email" type={'email'} name="email" placeholder="john.doe@example.com" />
                     <Errors errors={state.errors.fieldErrors.email} />
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                     <Label htmlFor="password">Password</Label>
-                    <Input data-testid="password" id="password" type={'password'} name="password" />
+                    <Input required={true} data-testid="password" id="password" type={'password'} name="password" />
                     <Errors errors={state.errors.fieldErrors.password} />
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                     <Label htmlFor="confirm-password">Confirm password</Label>
-                    <Input data-testid="confirm-password" id="confirm-password" type={'password'} name="confirm-password" />
+                    <Input required={true} data-testid="confirm-password" id="confirm-password" type={'password'} name="confirm-password" />
                     <Errors errors={state.errors.fieldErrors.confirmPassword} />
                 </div>
                 <FormButton textContent={'Register'} />
