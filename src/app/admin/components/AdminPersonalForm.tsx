f6496b34f8b3dc4b884ca608ba6950b92b1b97a6
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-10 23:11:48 +0530

    feat[admin]: added new AdminPersonalForm client component

---
+++ src/app/admin/components/AdminPersonalForm.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { Label } from "@radix-ui/react-label"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+
+export const AdminPersonalForm: React.FC = () => {
+    return <form className="mt-5 flex flex-col gap-4">
+    <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="firstname">First name</Label>
+        <Input className="w-full" type="text" id="firstname" name="firstname" placeholder="First name" />
+    </div>
+    <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="lastname">Last name</Label>
+        <Input type="text" id="lastname" name="lastname" placeholder="Last name" />
+    </div>
+    <div className="grid w-full items-center gap-1.5">
+        <Label htmlFor="description">Description</Label>
+        <Textarea name="description" />
+    </div>
+    <Button type="button" className="bg-zinc-800">Add paragraph</Button>
+    <Button className="bg-zinc-800  text-gray-200 duration-300" type="submit">Save</Button>
+</form>
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,30 +2,16 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
+import { AdminPersonalForm } from "./components/AdminPersonalForm"
 
 export default function Page() {
-    return <main className="horizon-page items-start">
-        <section className="px-20">
+    return <main className="horizon-page items-start w-2/4">
+        <section className="px-20 w-10/12">
             <div className="flex gap-2 flex-col">
                 <h1 className="text-3xl font-bold">Admin Dashboard</h1> 
                 <h2 className="text-md font-semibold text-gray-300 mt-3">Personal information</h2>
             </div>
-            <form className="mt-5 flex flex-col gap-4">
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <Label htmlFor="firstname">First name</Label>
-                    <Input type="text" id="firstname" name="firstname" placeholder="First name" />
-                </div>
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <Label htmlFor="lastname">Last name</Label>
-                    <Input type="text" id="lastname" name="lastname" placeholder="Last name" />
-                </div>
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <Label htmlFor="description">Description</Label>
-                    <Textarea name="description" />
-                </div>
-                <Button className="bg-zinc-800">Add paragraph</Button>
-                <Button className="bg-zinc-800  text-gray-200 duration-300" type="submit">Save</Button>
-            </form>
+            <AdminPersonalForm />
         </section>
     </main>
 }
