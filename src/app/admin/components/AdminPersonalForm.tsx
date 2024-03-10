4df3652971f625db5b13d1263cbea47af7d7ce5f
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-10 23:35:05 +0530

    feat[admin]: going to admin page with show the default values for the personal information form after fetching them from db

--- src/app/admin/components/AdminPersonalForm.tsx
+++ src/app/admin/components/AdminPersonalForm.tsx
@@ -4,16 +4,17 @@ import { Label } from "@radix-ui/react-label"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
+import { User } from "@prisma/client"
 
-export const AdminPersonalForm: React.FC = () => {
+export const AdminPersonalForm: React.FC<{ user: User }> = ({ user }) => {
     return <form className="mt-5 flex flex-col gap-4">
     <div className="grid w-full items-center gap-1.5">
         <Label htmlFor="firstname">First name</Label>
-        <Input className="w-full" type="text" id="firstname" name="firstname" placeholder="First name" />
+        <Input className="w-full" type="text" id="firstname" name="firstname" placeholder="First name" defaultValue={user.firstname} />
     </div>
     <div className="grid w-full items-center gap-1.5">
         <Label htmlFor="lastname">Last name</Label>
-        <Input type="text" id="lastname" name="lastname" placeholder="Last name" />
+        <Input type="text" id="lastname" name="lastname" defaultValue={user.lastname} placeholder="Last name" />
     </div>
     <div className="grid w-full items-center gap-1.5">
         <Label htmlFor="description">Description</Label>
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,27 @@
+import { getSession } from "@/auth/utils"
 import { AdminPersonalForm } from "./components/AdminPersonalForm"
+import prisma from "@/db";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+
+    const session = await getSession();
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: session?.userId
+        }
+    })
+
+    if(!user) redirect('/login');
+
     return <main className="horizon-page items-start w-2/4">
         <section className="px-20 w-10/12">
             <div className="flex gap-2 flex-col">
                 <h1 className="text-3xl font-bold">Admin Dashboard</h1> 
                 <h2 className="text-md font-semibold text-gray-300 mt-3">Personal information</h2>
             </div>
-            <AdminPersonalForm />
+            <AdminPersonalForm user={user} />
         </section>
     </main>
 }
